3f7c64ae9f00357b80e2ad16857fef94a6c09ed0
N1i2s3 <99121047+N1i2s3@users.noreply.github.com>
2022-02-16 14:31:17 +0530

    Add files via upload

---
+++ Speech.py
@@ -0,0 +1,66 @@
+import datetime
+import webbrowser
+import pyttsx3
+import speech_recognition as sr
+import wikipedia
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+#print(voices[1].id)
+engine.setProperty('voice', voices[1].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<=12:
+        speak("Good Morning")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon")
+
+    else:
+        speak("Good Evening")
+
+    speak("I am Nisarga")
+
+def takeCommand():
+    #It take microphone input from user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening.....")
+        r.pause_threshold=1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing....")
+        query = r.recognize_google(audio, language='en-in')
+        print("User Said:", query)
+        #print(f"User said:  {query}\n")
+
+    except Exception as e:
+        print(e)
+        print("say that again Please....")
+        return  "None"
+    return query
+
+if __name__ == "__main__":
+    wishMe()
+    #while True:
+    if 1:
+        query = takeCommand().lower()
+
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=1)
+            print(results)
+            speak("According to Wikipedia")
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
